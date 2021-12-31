c1422c24f5dea2e3756b5d37acb6f6d0b3c289f6
jmferg11 <jfergie93@gmail.com>
2021-12-31 11:57:29 -0500

    build newsfeed component

--- src/components/newsFeed.js
+++ src/components/newsFeed.js
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const NewsFeed = () => {
+  const [articles, setArticles] = useState(null);
+
+    useEffect(() => {
+      const options = {
+        method: 'GET',
+        url: 'https://crypto-news-live.p.rapidapi.com/news/coindesk',
+        headers: {
+          'x-rapidapi-host': 'crypto-news-live.p.rapidapi.com',
+          'x-rapidapi-key': ''
+        }
+      };
+      
+      axios.request(options).then((response) => {
+        console.log(response.data);
+        setArticles(response.data);
+      }).catch((error) => {
+        console.error(error);
+      });
+    }, []);
+
+    const firstBatch = articles?.slice(0,8);
+
     return (
       <div className="news-feed">
-        news feed
+
+        {firstBatch.map( (i, _index) => (
+          <a href={i.url} target='_blank'><p key={_index}>{i.title}</p></a>
+        ))}
+        
       </div>
     );
 }
